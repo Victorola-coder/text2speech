0a0bffea44560f5af583afb9296b4c245c6e3730
Victor || VickyJay <77025670+Victorola-coder@users.noreply.github.com>
2024-06-23 21:58:25 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Init SpeechSynth API
 const synth = window.speechSynthesis;
+
 // DOM Elements
 const textForm = document.querySelector("form");
 const textInput = document.querySelector("#text-input");
@@ -9,8 +10,10 @@ const rateValue = document.querySelector("#rate-value");
 const pitch = document.querySelector("#pitch");
 const pitchValue = document.querySelector("#pitch-value");
 const body = document.querySelector("body");
+
 // Init voices array
 let voices = [];
+
 const getVoices = () => {
   voices = synth.getVoices();
   // Loop through voices and create an option for each one
